4ca315e88f8ed230a58c65fdc1ca567074572359
Kverndal Emil Lysholm <X3F@tka.local>
2016-10-19 22:02:37 +0200

    WIP

---
+++ lib/fileutils.ex
@@ -0,0 +1,67 @@
+defmodule ArchivalConverters.Fileutils do
+    @moduledoc false
+
+    def path_is_type(path, type) do
+        case File.open(path) do
+            {:ok, io_dev} ->
+                case IO.binread(io_dev, 64) do
+                    {:error, reason} -> IO.puts("Error reading from file: #{path} #{reason}")
+                    :eof -> IO.puts("Error reading from file: #{path} EOF encountered before header was read")
+                    data ->
+                        File.close(io_dev)
+                        case detect_filetype(data) do
+                            {:ok, filetype} -> filetype == type
+                            {:error, _} ->
+                                IO.puts("Cannot recognize filetype for: #{path}")
+                                false
+                        end
+                end
+            {:error, posix} ->
+                IO.puts("Error opening file: #{path} #{posix}")
+        end
+    end
+
+    def path_is_perhaps_office_file(path) do
+        path_is_type(path, "ZIP") or path_is_type(path, "CFBF")
+    end
+
+    def recursive_ls(path) do
+        cond do
+            File.regular?(path) -> [path]
+            File.dir?(path) ->
+                File.ls!(path) |>
+                Enum.map(&Path.join(path, &1)) |>
+                Enum.map(&recursive_ls/1) |>
+                Enum.concat
+            true -> []
+        end
+    end
+
+    def extract_archive(archive_path, extract_path, output_path) do
+        IO.puts "Extracting archive"
+        System.cmd("7z", ["x", archive_path, extract_path, "-aou", "-o#{output_path}"])
+        Path.absname(Path.join(output_path, extract_path))
+    end
+
+    def update_archive(archive_path, update_path, cd) do
+        IO.puts "Updating archive"
+        System.cmd("7z", ["u", archive_path, update_path], cd: cd)
+        archive_path
+    end
+
+    @spec detect_filetype(binary) :: {atom, String.t}
+    def detect_filetype("%PDF" <> _), do: {:ok, "PDF"}
+    def detect_filetype(<<0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1, _ :: binary>>), do: {:ok, "CFBF"}
+    def detect_filetype(<<0x50, 0x4B, 0x03, 0x04, _ :: binary>>), do: {:ok, "ZIP"}
+    def detect_filetype("BM" <> _), do: {:ok, "BMP"}
+    def detect_filetype(<<0xFF, 0xD8, 0xFF, _ :: binary>>), do: {:ok, "JPEG"}
+    def detect_filetype(<<0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, _ :: binary>>), do: {:ok, "JPEG2000"}
+    def detect_filetype(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _ :: binary>>), do: {:ok, "PNG"}
+    def detect_filetype(unkown) do
+        cond do
+            String.valid?(unkown) -> {:ok, "UTF-8"}
+            true -> <<head :: size(64), _ :: binary>> = unkown
+                {:error, <<head :: size(64)>>}
+        end
+    end
+end
--- lib/siard.ex
+++ lib/siard.ex
@@ -1,8 +1,30 @@
 defmodule ArchivalConverters.Siard do
     @moduledoc false
 
-    def update_header(a, b) do
-      IO.inspect a
-      IO.inspect b
+    @metadata_path "header/metadata.xml"
+
+    def update_header(siard_path, description_path) do
+        descriptions = File.read!(description_path) |> ArchivalConverters.Yaml.decode
+        cond do
+            File.dir?(siard_path) ->
+                update_metadata({"", @metadata_path, siard_path}, descriptions)
+            ArchivalConverters.Fileutils.path_is_type(siard_path, "ZIP") ->
+                case System.tmp_dir() do
+                    nil ->
+                        IO.puts "Cannot create temp dir, check system priveleges."
+                        System.halt(1)
+                    temp_path ->
+                        IO.puts temp_path
+                        ArchivalConverters.Fileutils.extract_archive(siard_path, @metadata_path, temp_path)
+                            |> update_metadata(descriptions)
+                        ArchivalConverters.Fileutils.update_archive(siard_path, @metadata_path, temp_path)
+                end
+            true -> IO.puts("#{siard_path} is not a folder or siard archive")
+        end
+    end
+
+    def update_metadata(path, descriptions) do
+        IO.inspect path
+        path
     end
 end
