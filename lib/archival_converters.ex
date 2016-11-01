180cd4dadd9238e964343a69f35180309105a1dd
Kverndal Emil Lysholm <X3F@tka.local>
2016-11-01 13:24:16 +0100

    last warning

--- lib/archival_converters.ex
+++ lib/archival_converters.ex
@@ -3,16 +3,16 @@ defmodule ArchivalConverters do
     alias ArchivalConverters.Fileutils
     alias ArchivalConverters.Siard
 
-    def main(["doc-to-pdfa", path | _]) do
-        System.cmd(Path.absname("lib\\doc_to_pdfa.js"), find_files(path))
-    end
-
     def find_files(path) do
         path |>
             Fileutils.recursive_ls |>
             Enum.filter(&Fileutils.path_is_perhaps_office_file(&1)) |>
             Enum.filter(fn(p) -> String.ends_with?(p, "doc") end) |>
             Enum.map(fn(p) -> String.replace(p, "/", "\\") end)
+    end
+
+    def main(["doc-to-pdfa", path | _]) do
+        System.cmd(Path.absname("lib\\doc_to_pdfa.js"), find_files(path))
     end
 
     def main(["update-siard", siard_path, yaml_path | _]) do
